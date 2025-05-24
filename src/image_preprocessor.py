72c18b78371ecb91db7d6a2d553e71134b30cdc6
Mr.Benício <lbeniciosilva@gmail.com>
2025-05-24 13:32:05 -0300

    Update image_preprocessor.py

--- src/image_preprocessor.py
+++ src/image_preprocessor.py
@@ -1,10 +1,11 @@
 import numpy as np
 from tensorflow.keras.preprocessing import image
+import tensorflow as tf
 
 def preprocess_image(img_path):
-  
+
     img = image.load_img(img_path, target_size=(128, 128))
     img_array = image.img_to_array(img)
-    img_array = img_array / 255.0
-    img_final = np.expand_dims(img_array, axis=0)
+    img_array = np.expand_dims(img_array, axis=0)
+    img_final = tf.keras.applications.mobilenet_v2.preprocess_input(img_array)
     return img_final
